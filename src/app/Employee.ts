3ed8ec86b0e10c84304dbdce96564b377daa5792
Ahmed-Abbah <Abbah.Ahmed@etu.uae.ac.ma>
2023-10-07 23:56:40 +0100

    Ahmed's Commit

---
+++ src/app/Employee.ts
@@ -0,0 +1,8 @@
+export interface Employee {
+    id : number;
+    name : String;
+    email : string;
+    JobTitle : String;
+    phone : string;
+    employeeCode : String;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './Employee';
+import { EmployeeService } from './employee.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EM_FE';
+export class AppComponent implements OnInit{
+
+  ngOnInit() {
+      this.getEmployees();
+  }
+  title = 'Employees Management App';
+  public employees : Employee[]|undefined;
+  constructor(private employeeService: EmployeeService){
+  }
+
+
+
+  public getEmployees():void{
+    this.employeeService.getEmployees().subscribe(
+      { 
+        next: (response) => {
+          this.employees = response;
+        },
+        error:(error) => {
+          alert(error.message);
+        }
+      }
+    )
+  }
 }
